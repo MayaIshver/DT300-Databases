02a918938441513aad844b0639eaa79f83b07483
MayaIshver <110359450+MayaIshver@users.noreply.github.com>
2022-09-14 13:44:31 +1200

    Changes #1
    Adding main page aspects, about us section, main donate/search section and the top fundraiser's section. Not correctly formated

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+	//declaring php file
+
+	
+
+	/* Connect to database */
+
+	$dbcon = mysqli_connect("localhost", "mayaishver", "YjHS6Z8", "mayaishver_fundraiser");
+	if ($dbcon == NULL) {
+		echo "Database is not connected.";
+		exit();
+	}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title> Funderly </title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+	</head>
+	<body>
+		<header>
+			<h1> Funderly </h1>
+			<a href="login.html" class="button1">Login/sign-up</a>
+		</header>
+		<!-- hamburger -->
+		<input type="checkbox" id="navi-toggle" class="checkbox" />
+		<label for="navi-toggle" class="button">
+			<span class="icon">&nbsp;</span>
+		</label>
+		<div class="background">&nbsp;</div>
+		<!-- nav -->
+		<nav class="nav">
+			<ul class="list">
+				<li class="item">
+					<a class="link"> Search </a>
+				</li>
+				<li class="item">
+					<a class="link"> Login </a>
+				</li>
+				<li class="item">
+					<a class="link"> About </a>
+				</li>
+				<li class="item">
+					<a class="link"> My Account </a>
+				</li>
+			</ul>
+		</nav>
+		<section id="about_intro">
+			<article>
+				<h3> What is Funderly?</h3>
+				<p> Funderly is an online charity platform. It is similar to sites like 'go fund me' and 'pledge me'. The site allows people wanting a charity to do so and make an account while also allowing donors to donate without an account.<p>
+				
+				<p> For more info check out the ABOUT page linked below and in the nav bar. </p>
+				
+				<a href="about.html" class="button2">About</a>
+				
+			</article>
+		</section>
+		<section id="donate">
+			<p></p>
+			<div class="search_block">
+				<a href="search.html" class="button2">Donate Now!</a>
+				<input type="text" placeholder="Search..">
+			</div>
+		</section>
+		
+		<section id="top_fund">
+			<h3>Top 5 Charities</h3>
+			<div class="grid-container">
+				<div class="grid-item">charity</div>
+				<a href="charity.html" class="grid-button">Pledge!</a>
+				<div class="grid-item">charity</div>
+				<a href="charity.html" class="grid-button">Pledge!</a>
+				<div class="grid-item">charity</div>
+				<a href="charity.html" class="grid-button">Pledge!</a>
+				<div class="grid-item">charity</div>
+				<a href="charity.html" class="grid-button">Pledge!</a>
+				<div class="grid-item">charity</div>
+				<a href="charity.html" class="grid-button">Pledge!</a>
+			
+	</body>
+</html>
